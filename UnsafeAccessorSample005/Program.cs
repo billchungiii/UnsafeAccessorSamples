b8dbccf19d473f3957d6fa7d0696a19f131e8aa0
Bill Chung <bill_chungiii@hotmail.com>
2025-12-07 07:37:01 +0800

    UnsafeAccessorSample005 completed

---
+++ UnsafeAccessorSample005/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace UnsafeAccessorSample005
+{
+    /// <summary>
+    /// .NET9 support generic UnsafeAccessor
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var list = GenericAccessor<int>.Create();
+            Console.WriteLine($"list count {list.Count}");
+
+                       
+            GenericAccessor<int>.Add(list, 999);
+            Console.WriteLine($"list count {list.Count}, index 0 is {list[0]}");
+
+        }
+    }
+
+    public static class GenericAccessor<T>
+    {
+        /// <summary>
+        /// Accessing public constructor of generic class List<T>
+        /// </summary>
+        /// <returns></returns>
+        [UnsafeAccessor(UnsafeAccessorKind.Constructor)]
+        public extern static List<T> Create();
+        /// <summary>
+        /// Accessing public method Add of generic class List<T>
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="item"></param>
+        [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "Add")]
+        public extern static void Add(List<T> list, T item);
+    }
+}
