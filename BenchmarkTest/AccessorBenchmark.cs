566f17be5ce683e48ab2d97ca9909bb2984990b9
Bill Chung <bill_chungiii@hotmail.com>
2025-12-06 21:53:31 +0800

    add benchmark test

---
+++ BenchmarkTest/AccessorBenchmark.cs
@@ -0,0 +1,60 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BenchmarkTest
+{
+    [MemoryDiagnoser]
+    public class AccessorBenchmark
+    {
+        private Person _person;
+        [GlobalSetup]
+        public void Setup()
+        {
+            _person = new Person("BenchmarkUser", 40);
+        }
+
+        [Benchmark(Description = "Field — direct access")]
+        public void AccessPrivateField_Direct()
+        {            
+            _person._age += 1;
+        }
+
+        [Benchmark(Description = "Field — unsafeaccessor")]
+        public void AccessPrivateField_Unsafe()
+        {
+            ref int age = ref PersonAccessor.GetAgeField(_person);
+            age += 1;
+        }
+
+        [Benchmark(Description = "Field — reflection")]
+        public void AccessPrivateField_Reflection()
+        {
+            var fieldInfo = typeof(Person).GetField("_age");
+            int age = (int)fieldInfo.GetValue(_person);
+            age += 1;
+            fieldInfo.SetValue(_person, age);
+        }
+
+        [Benchmark(Description = "Method — direct access")]
+        public void AccessPrivateMethod_Direct()
+        {
+            _person.AddAge(2);
+        }
+
+        [Benchmark(Description = "Method — unsafeaccessor")]
+        public void AccessPrivateMethod_Unsafe()
+        {
+            PersonAccessor.CallAddAge(_person, 2);
+        }
+
+        [Benchmark(Description = "Method — reflection")]
+        public void AccessPrivateMethod_Reflection()
+        {
+            var methodInfo = typeof(Person).GetMethod("AddAge");
+            methodInfo.Invoke(_person, new object[] { 2 });
+        }
+
+    }
+}
---
+++ BenchmarkTest/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace BenchmarkTest
+{
+
+    public class Person
+    {
+        /// <summary>
+        /// field
+        /// </summary>
+        public string _name;
+
+        /// <summary>
+        /// filed
+        /// </summary>
+        public int _age;
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="age"></param>
+        public Person(string name, int age) => (_name, _age) = (name, age);
+       
+        /// <summary>
+        /// method (with parameters)
+        /// </summary>
+        /// <param name="year"></param>
+        public void AddAge(int year) => _age += year;
+    }
+
+    public static class PersonAccessor
+    {
+        /// <summary>
+        /// Access private instance constructor
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="age"></param>
+        /// <returns></returns>
+        [UnsafeAccessor(UnsafeAccessorKind.Constructor)]
+        public extern static Person Create(string name, int age); 
+
+        /// <summary>
+        /// Access private instance method (AddAge)
+        /// </summary>
+        /// <param name="person"></param>
+        /// <param name="year"></param>
+        [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "AddAge")]
+        public extern static void CallAddAge(Person person, int year);
+
+
+        /// <summary>
+        /// Access private instance field (_name)
+        /// </summary>
+        /// <param name="person"></param>
+        /// <returns></returns>
+        [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_name")]
+        public extern static ref string GetNameFiled(Person person);
+
+        /// <summary>
+        /// Access private instance field (_age)
+        /// </summary>
+        /// <param name="person"></param>
+        /// <returns></returns>
+        [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_age")]
+        public extern static ref int GetAgeField(Person person);
+
+    }
+}
