32cd2fdd5accef7e0b4b2111c4111cb663fddc69
Bill Chung <bill_chungiii@hotmail.com>
2025-12-06 16:07:47 +0800

    UnsafeAccessorSample001 completed

--- UnsafeAccessorSample001/Program.cs
+++ UnsafeAccessorSample001/Program.cs
@@ -1,10 +1,94 @@
-﻿namespace UnsafeAccessorSample001
+﻿using System.Runtime.CompilerServices;
+
+namespace UnsafeAccessorSample001
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var person = PersonAccessor.Create("Joe", 25);
+            PersonAccessor.CallDisplay(person);
+            ref int age = ref PersonAccessor.GetAgeField(person);
+            age = 31;
+            PersonAccessor.CallDisplay(person);
+            ref string name = ref PersonAccessor.GetNameFiled(person);
+            name = "David";
+            PersonAccessor.CallDisplay(person);
+            string description = PersonAccessor.CallDescribe(person);
+            Console.WriteLine(description);
+
+            PersonAccessor.CallAddAge(person, 10);
+            PersonAccessor.CallDisplay(person);
+
         }
     }
+
+    public class Person
+    {
+        private string _name;
+        private int _age;
+
+        private Person(string name, int age) => (_name, _age) = (name, age);
+
+        private void Display() => Console.WriteLine($"{_name} -- {_age}");
+
+        private string Describe() => $"My name is {_name} ,and I am {_age} years old";
+
+        private void AddAge(int year) => _age += year;
+    }
+
+    public class PersonAccessor
+    {
+        /// <summary>
+        /// Access private instance constructor
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="age"></param>
+        /// <returns></returns>
+        [UnsafeAccessor(UnsafeAccessorKind.Constructor)]
+        public extern static Person Create(string name, int age);
+
+        /// <summary>
+        /// Access private instance method (Display)
+        /// </summary>
+        /// <param name="person"></param>
+        [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "Display")]
+        public extern static void CallDisplay(Person person);
+
+        /// <summary>
+        /// Access private instance method (Describe)
+        /// </summary>
+        /// <param name="person"></param>
+        /// <returns></returns>
+        [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "Describe")]
+        public extern static string CallDescribe(Person person);
+
+
+        /// <summary>
+        /// Access private instance method (AddAge)
+        /// </summary>
+        /// <param name="person"></param>
+        /// <param name="year"></param>
+        [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "AddAge")]
+        public extern static void CallAddAge(Person person, int year);
+
+
+        /// <summary>
+        /// Access private instance field (_name)
+        /// </summary>
+        /// <param name="person"></param>
+        /// <returns></returns>
+        [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_name")]
+        public extern static ref string GetNameFiled(Person person);
+
+        /// <summary>
+        /// Access private instance field (_age)
+        /// </summary>
+        /// <param name="person"></param>
+        /// <returns></returns>
+        [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_age")]
+        public extern static ref int GetAgeField(Person person);
+
+    }
+
 }
