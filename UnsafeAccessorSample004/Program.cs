068de5c86cdabec41b95d41d58c6e6c91d4f357c
Bill Chung <bill_chungiii@hotmail.com>
2025-12-07 07:17:01 +0800

    modify comments

--- UnsafeAccessorSample004/Program.cs
+++ UnsafeAccessorSample004/Program.cs
@@ -12,11 +12,11 @@ namespace UnsafeAccessorSample004
         static void Main(string[] args)
         {
             // create List<int> instance via UnsafeAccessor (.NET8 success)
-            // if you change TargetFramework to net9.0, it will thorw exception System.InvalidProgramException: Generic type constraints do not match.
+            // if you change TargetFramework to net9.0, it will thorw exception -- System.InvalidProgramException: Generic type constraints do not match.
             var list = GenericAccessor.Create();
             Console.WriteLine($"list count {list.Count}");
 
-            // this will throw exception System.MissingMethodException: Method not found: System.Collections.Generic.List`1.Add.            
+            // this will throw exception -- System.MissingMethodException: Method not found: System.Collections.Generic.List`1.Add.            
             GenericAccessor.Add(list, 999);
             Console.WriteLine($"list count {list.Count}, index 0 is {list[0]}");
         }
