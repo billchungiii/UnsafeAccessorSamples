dadc78a1d6671e9aa57b18ffeca351d31fb02a63
Bill Chung <bill_chungiii@hotmail.com>
2025-12-06 17:37:30 +0800

    add UnsafeAccessorSample002 proj

---
+++ UnsafeAccessorSample002/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace UnsafeAccessorSample002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    public class Person
+    {
+        private string Name { get; set; }
+        private int Age { get; set; }
+    }
+}
