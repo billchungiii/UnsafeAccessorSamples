4763e0366e4655e776bd6931d259b5c38be509ff
Bill Chung <bill_chungiii@hotmail.com>
2025-12-07 08:33:24 +0800

    add 006, 007

---
+++ UnsafeAccessorSample006/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace UnsafeAccessorSample006
+{
+    /// <summary>
+    /// .NET 10 support accessing private nested class via UnsafeAccessor by UnsafeAccessorType attribute
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ref var innerInstance = ref OuterClassAccessor.GetInnerInstance(new OuterClass());
+            OuterClassAccessor.CallShowMessage(innerInstance);
+        }
+    }
+
+
+    public class OuterClass
+    {
+        private object _innerInstance = new InnerClass();
+        /// <summary>
+        /// this is an nested inner private class
+        /// </summary>
+        private class InnerClass
+        {
+            private void ShowMessage()
+            {
+                Console.WriteLine("Hello from InnerClass!");
+            }
+        }
+    }
+
+    public static class OuterClassAccessor
+    {
+        /// <summary>
+        /// Accessing private field _innerInstance of OuterClass
+        /// </summary>
+        /// <param name="outerInstance"></param>
+        /// <returns></returns>
+        [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_innerInstance")]
+        public extern static ref object GetInnerInstance(OuterClass outerInstance);
+
+        /// <summary>
+        /// Accessing private nested class InnerClass's method ShowMessage 
+        /// </summary>
+        /// <param name="innerInstance"></param>
+        [UnsafeAccessor(UnsafeAccessorKind.Method, Name = "ShowMessage")]
+        public extern static void CallShowMessage([UnsafeAccessorType("UnsafeAccessorSample006.OuterClass+InnerClass")] object instance);
+    }
+}
---
+++ UnsafeAccessorSample007/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace UnsafeAccessorSample007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sqrt = MathAccessor.Sqrt(null, 16.0);
+            Console.WriteLine($"The square root of 16.0 is {sqrt}");
+            var pow = MathAccessor.Pow(null, 2.0, 8.0);
+            Console.WriteLine($"2.0 to the power of 8.0 is {pow}");
+        }
+    }
+
+    public static class MathAccessor
+    {
+        /// <summary>
+        /// Accessing static method Sqrt of Math class
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [UnsafeAccessor(UnsafeAccessorKind.StaticMethod, Name = "Sqrt")]
+        public extern static double Sqrt([UnsafeAccessorType("System.Math")] object _, double value);
+
+
+        /// <summary>
+        /// Accessing static method Pow of Math class
+        /// </summary>
+        /// <param name="_"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        [UnsafeAccessor(UnsafeAccessorKind.StaticMethod, Name = "Pow")]
+        public extern static double Pow([UnsafeAccessorType("System.Math")] object _, double x, double y);
+    }
+}
